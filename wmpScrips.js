7d1f732b00508ef69a61d07dff8da25c5843d391
Emanuel Fabio <efabio05@icloud.com>
2023-02-21 20:43:33 -0800

    Read Data functionality Added function that reads data from txt file and displays it screen. Also did minor changes to css function to. Added timer and title to html as well.

--- wmpScrips.js
+++ wmpScrips.js
@@ -1,11 +1,34 @@
 "use strict";
 
 const wordArea = document.querySelector(".test");
-const words = ["Apple", "Dog", "Cat", "Orange", "House", "Car", "Bottle", "Frog"];
 
-wordArea.textContent = "";
-
-words.forEach(word => 
+const wpm = 
 {
-    wordArea.textContent += `${word} `;
-});
+    wordData: [],
+
+    /*Read Data from text file*/
+    async readDataIntoArray(){
+        this.wordData = await fetch("wordData.txt")
+        .then(response => response.text())
+        .then(data => {
+            return data.match(/\b(\w+)\b/g);
+        });
+    },
+    updateWordDisplay()
+    {   
+        wordArea.textContent = "";
+        this.readDataIntoArray();
+        function display()
+        {
+            wpm.wordData.forEach(word => 
+            {
+                wordArea.textContent += `${word} `;
+            });
+        }
+        // Required while data is being stored in array
+        setTimeout(display, 5);
+    }
+}
+
+wpm.updateWordDisplay();
+
